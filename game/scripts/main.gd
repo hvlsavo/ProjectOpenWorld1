64dd3330cb3d1959665bde1d6f253c7cb9604410
Lone Walker Studios <66659302+mickTrumble@users.noreply.github.com>
2023-08-14 21:49:34 -0500

    AnotherChange
    POP

--- game/scripts/main.gd
+++ game/scripts/main.gd
@@ -3,6 +3,7 @@ extends Node3D
 class_name Main_
 
 @onready var SoundManager: SoundManager_ = $"[SoundManager]"
+@onready var DefaultPlayerSpawn = $Player_defaultSpawnPoint
 
 @export var NpcsPack: Array[PackedScene]
 @export var PlayerPack: PackedScene
@@ -19,8 +20,11 @@ func Load_MainMenu():
 	pass
 
 func Load_World():
+	DefaultPlayerSpawn.visible = false
 	Player = PlayerPack.instantiate()
 	add_child(Player)
+	Player.position = DefaultPlayerSpawn.position
+	Player.rotation = DefaultPlayerSpawn.rotation
 
 func Spawn_Npc(Spawn_Position: Vector3, Spawn_Rotation: Vector3):
 	Player.position = Spawn_Position
